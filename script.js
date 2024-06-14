46c3b7abfb4fece5e5ae078ad0035c4e61c8112c
Hachu04 <106154916+Hachu04@users.noreply.github.com>
2024-06-14 13:02:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function clearDisplay() {
+    document.getElementById('display').innerText = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.innerText = display.innerText.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    const display = document.getElementById('display');
+    display.innerText += character;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (error) {
+        display.innerText = 'Error';
+    }
+}
